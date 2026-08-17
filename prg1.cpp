3bcce079d1bc23a09804f7307c399c52fcd94f88
Dhruv Gupta <babitagupta76834@gmail.com>
2026-08-17 11:36:24 +0530

    prg1

---
+++ prg1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// Student class
+class Student
+{
+private:
+    int rollNo;
+    string name;
+    float marks;
+
+public:
+    void input()
+    {
+        cout << "Enter Roll Number: ";
+        cin >> rollNo;
+
+        cin.ignore();
+
+        cout << "Enter Name: ";
+        getline(cin, name);
+
+        cout << "Enter Marks: ";
+        cin >> marks;
+    }
+
+    void display()
+    {
+        cout << "\n------ Student Record ------" << endl;
+        cout << "Roll Number : " << rollNo << endl;
+        cout << "Name        : " << name << endl;
+        cout << "Marks       : " << marks << endl;
+    }
+};
+
+int main()
+{
+    Student s;
+
+    s.input();
+    s.display();
+
+    return 0;
+}
